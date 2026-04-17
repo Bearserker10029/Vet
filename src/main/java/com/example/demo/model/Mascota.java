7ee7cd34b7dc67f767d08044a09cc3b0eeaecafc
kent <a20202132@pucp.edu.pe>
2026-04-16 20:59:25 -0500

    pregunta 4

--- src/main/java/com/example/demo/model/Mascota.java
+++ src/main/java/com/example/demo/model/Mascota.java
@@ -20,19 +20,25 @@ public class Mascota {
     @Column(name = "id")
     private int id;
 
+    @Column(name="nombre")
     private String nombre;
 
+    @Column(name = "especie")
     private String especie;
 
+    @Column(name = "raza")
     private String raza;
 
+    @Column(name = "edad")
     private int edad;
 
     @Column(name = "nombre_dueno")
     private String nombreDuenio;
 
+    @Column(name = "telefono")
     private String telefono;
 
+    @Column(name = "estado")
     private int estado;
 
 }
